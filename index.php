702e2222a7447d91095e2eef920c78a95feb663c
evikza <48568977+evikza@users.noreply.github.com>
2021-08-30 22:12:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php	
+<?php
 
-$uri = $_GET['uri'];
+$uri = filter_var(@$_GET['uri'], FILTER_VALIDATE_URL)
+  ? @$_GET['uri']
+  : die(json_encode(['error' => 'Could not determine URI parameter']));
 
-echo $uri;
+$response = file_get_contents(
+  $uri,
+  0,
+  stream_context_create([
+    'http' => [
+      'method' => 'GET',
+      'header' =>
+        'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    ],
+  ])
+);
+
+echo $response;
