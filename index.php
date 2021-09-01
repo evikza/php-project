548a9ffddf47c8a1b7224d0aef2c26fdddf7f7b6
evikza <48568977+evikza@users.noreply.github.com>
2021-09-01 18:10:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $context = stream_context_create([
   'http' => [
     'header' => ['Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'],
-    'proxy' => 'tcp://51.158.169.52:29976', # tcp://51.158.169.52:29976
+//     'proxy' => 'tcp://51.158.169.52:29976', # tcp://51.158.169.52:29976
     // 'request_fulluri' => true,
   ],
 ]);
