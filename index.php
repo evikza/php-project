ccde0ea568e8281486926dbaa55783fa63b4c5a3
evikza <48568977+evikza@users.noreply.github.com>
2022-10-18 16:31:13 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
 $uri = filter_var(@$_GET['uri'], FILTER_VALIDATE_URL)
   ? @$_GET['uri']
   : die(json_encode(['error' => 'Could not determine URI parameter']));
